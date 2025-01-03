dcb780f84ecf6d2c62a4e6b84637dc0a1aeef591
Abdu Rangga Senjari <abduranggasenjari@gmail.com>
2025-01-03 09:44:45 +0700

    remove author field in Page and News

--- resources/views/admin/page/show.blade.php
+++ resources/views/admin/page/show.blade.php
@@ -54,7 +54,7 @@
                                 </tr>
                                 <tr>
                                     <td class="text-nowrap"><i class="ph-user me-2"></i>Penulis:</td>
-                                    <td class="text-end">{{ $page->author->name }}</td>
+                                    <td class="text-end">{{ $page->user->name }}</td>
                                 </tr>
                                 <tr>
                                     <td class="text-nowrap"><i class="ph-calendar-blank me-2"></i>Posting:</td>
